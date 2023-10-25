4c1d8ec0a3dac0ae221ccadf85c177d84582515d
Emmy Manning <eobcre@gmail.com>
2023-10-25 01:26:50 -0400

    fetch, useEffect settings and logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const App = () => {
   const [users, setUsers] = useState([]);
+
+  const fetchUsers = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    return res.json();
+  };
+
+  useEffect(() => {
+    fetchUsers().then((data) => {
+      setUsers(data);
+    });
+  }, []);
 
   return (
     <div>
